2587fcfc8cf79a03f941fed7dec1e32ebf890efd
mam587 <milan.makwana@york.ac.uk>
2020-10-15 14:13:01 +0100

    Update mergeBumps script

--- mergeBumps.sh
+++ mergeBumps.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-gh pr list --state open | grep -E "^[0-9]+\sBump" | cut -f 1 | sort | while read -r prId ; do
+gh pr list --state open | grep -E "^[0-9].*\bBump.*dependabot\b" | cut -f 1 | sort | while read -r prId ; do
 
   echo "Processing $prId"
 	retries=0
 
+  echo "Try code review"
+	gh pr review $prId --approve
+
 	while [ $retries -lt 3 ] 
 	do
+
 
     echo "Try merge $retries"
 		gh pr merge $prId --rebase
